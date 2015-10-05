a373bc3b18914eb366fa553df79bc88871b970bf
Aileen <acacayorin@gmail.com>
2015-10-05 14:09:20 -0700

    new daycare form function

--- app/routes/new-daycare.js
+++ app/routes/new-daycare.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+
+    addDaycare(params) {
+      debugger;
+      var newDaycare = this.store.createRecord('daycare', params);
+      newDaycare.save();
+      this.transitionTo('index');
+    }
+  }
 });
