5b2fb26349e343c382d92356a68685846835e887
Janice <janicelaset@gmail.com>
2015-10-05 14:25:31 -0700

    testing geocoding

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   map: Ember.inject.service('google-map'),
+  initMap: null,
 
   didInsertElement: function() {
       this.$('.search-daycare').hide();
@@ -20,7 +21,16 @@ export default Ember.Component.extend({
       this.$('.display-map').hide();
     },
     search() {
-      
+      var container = this.$('.map-display')[0];
+      var options = {
+        center: this.get('map').center(39.8282, -98.5795),
+        zoom: 4
+      };
+      var newMap = this.get('map').findMap(container, options);
+
+      var zip = this.get('zipCode');
+      var latLong = this.get('map').codeZip(newMap, zip);
+      debugger;
     }
   }
 });
--- app/services/google-map.js
+++ app/services/google-map.js
@@ -2,11 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   googleMaps: window.google.maps,
-  
+
   findMap(container, options) {
     return new this.googleMaps.Map(container, options);
   },
   center(latitude, longitude) {
     return new this.googleMaps.LatLng(latitude, longitude);
+  },
+  codeZip(map, zip) {
+    debugger;
+    var geocoder = new this.googleMaps.Geocoder();
+    geocoder.geocode( {'address': '6141 SW CrossCreek Dr, Aloha, OR'}, function(results, status) {
+      debugger;
+      if(status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        return results[0].geometry.location;
+      }
+      else {
+        alert("It didn't work because" + status);
+      }
+    });
   }
 });
