cab8ca36c54579d5fe5fb8de560813f085c55c53
Janice <janicelaset@gmail.com>
2015-10-05 13:42:48 -0700

    display map when user presses Find Daycare button

--- app/routes/daycare.js
+++ app/routes/daycare.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('daycare', param.daycare_id)
+    return this.store.findRecord('daycare', params.daycare_id);
   }
 });
