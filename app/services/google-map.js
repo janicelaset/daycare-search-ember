2ab9367265d87aeb204cc20022fe092474615d2a
Janice <janicelaset@gmail.com>
2015-10-06 12:32:26 -0700

    allow user to find daycare based on radius from address

--- app/services/google-map.js
+++ app/services/google-map.js
@@ -9,20 +9,60 @@ export default Ember.Service.extend({
   center(latitude, longitude) {
     return new this.googleMaps.LatLng(latitude, longitude);
   },
-  codeZip(map, zip) {
-    var geocoder = new this.googleMaps.Geocoder();
-    geocoder.geocode( {'address': zip}, function(results, status) {
-      if(status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        map.fitBounds(results[0].geometry.bounds)
-      }
-      else {
-        alert("It didn't work because" + status);
+  codeAddress(map, origin, addresses, radius) {
+    console.log(map);
+    console.log(origin);
+    console.log(addresses);
+    console.log(radius);
+    // var geocoder = new this.googleMaps.Geocoder();
+    //
+    // geocoder.geocode( {'address': address}, function(results, status) {
+    //   if(status == google.maps.GeocoderStatus.OK) {
+    //     map.setCenter(results[0].geometry.location);
+    //     map.fitBounds(results[0].geometry.bounds)
+    //   }
+    //   else {
+    //     alert("It didn't work because" + status);
+    //   }
+    //
+    // });
+    var withinRadius = [];
+    var service = new google.maps.DistanceMatrixService();
+    console.log('created service distance matrix');
+    service.getDistanceMatrix(
+    {
+      origins: [origin],
+      destinations: addresses,
+      travelMode: google.maps.TravelMode.DRIVING
+    }, function(response, status) {
+
+      if (status == google.maps.DistanceMatrixStatus.OK) {
+        // var origins = response.originAdddresses;
+        var destinations = response.destinationAddresses;
+
+        // for(var i = 0; i < origins.length; i) {
+          var results = response.rows[0].elements;
+          for (var j = 0; j < results.length; j++) {
+            var element = results[j];
+            var distance = element.distance.value;
+            // var duration = element.duration.value;
+            // var from = origins[i];
+            console.log(distance);
+            var to = destinations[j];
+            if (distance <= radius) {
+              withinRadius.push(to);
+              console.log(withinRadius);
+            }
+          }
+        // }
+    return withinRadius;
+
       }
     });
   },
   setMarkers(map, addresses) {
     var geocoder = new this.googleMaps.Geocoder();
+
     addresses.forEach(function(address) {
       geocoder.geocode( {'address': address}, function(results, status) {
         if(status == google.maps.GeocoderStatus.OK) {
